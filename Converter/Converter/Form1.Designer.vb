5c47fb6d43e9d7b62fb42f6903186821775f0db7
MrYetiGuy <32039460+MrYetiGuy@users.noreply.github.com>
2018-02-28 11:58:36 +0000

    Add files via upload

---
+++ Converter/Converter/Form1.Designer.vb
@@ -0,0 +1,255 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.MaskedTextBox1 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox2 = New System.Windows.Forms.MaskedTextBox()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(31, 132)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Convert"
+        Me.ToolTip1.SetToolTip(Me.Button1, "Click here to convert from £ to €")
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(152, 132)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Convert"
+        Me.ToolTip1.SetToolTip(Me.Button2, "Click here to convert from € to £")
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(15, 263)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "Help"
+        Me.ToolTip1.SetToolTip(Me.Button5, "Click me for help message")
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(95, 263)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "Get rate"
+        Me.ToolTip1.SetToolTip(Me.Button6, "Click me to get rate online" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10))
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(176, 263)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(75, 23)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "Clear"
+        Me.ToolTip1.SetToolTip(Me.Button7, "Click me to clear amount, result")
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(32, 116)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(65, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Convert to €"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(161, 116)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(65, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Convert to £"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 82)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(30, 13)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Rate"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 43)
+        Me.Label4.Name = "Label4"
+        Me.Label4.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.Label4.Size = New System.Drawing.Size(121, 13)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "Amount to be converted"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Century", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(8, 9)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(241, 18)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "Currency Converter Program"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 158)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(37, 13)
+        Me.Label6.TabIndex = 15
+        Me.Label6.Text = "Result"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(10, 238)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(79, 13)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "Further Options"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(14, 292)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 18
+        Me.Button3.Text = "Exit"
+        Me.ToolTip1.SetToolTip(Me.Button3, "Click me to exit the program")
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(95, 291)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(157, 23)
+        Me.Button4.TabIndex = 19
+        Me.Button4.Text = "Update Rate"
+        Me.ToolTip1.SetToolTip(Me.Button4, "Click me to save rate for later use.")
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'MaskedTextBox1
+        '
+        Me.MaskedTextBox1.Location = New System.Drawing.Point(139, 40)
+        Me.MaskedTextBox1.Mask = "0000000000000000000000000000000000000000"
+        Me.MaskedTextBox1.Name = "MaskedTextBox1"
+        Me.MaskedTextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.MaskedTextBox1.TabIndex = 20
+        Me.ToolTip1.SetToolTip(Me.MaskedTextBox1, "Enter the amount you want to convert")
+        '
+        'MaskedTextBox2
+        '
+        Me.MaskedTextBox2.Location = New System.Drawing.Point(48, 79)
+        Me.MaskedTextBox2.Mask = "0.00"
+        Me.MaskedTextBox2.Name = "MaskedTextBox2"
+        Me.MaskedTextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.MaskedTextBox2.TabIndex = 21
+        Me.ToolTip1.SetToolTip(Me.MaskedTextBox2, "Enter the rate you want to use in the conversion")
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(15, 175)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(249, 60)
+        Me.TextBox1.TabIndex = 22
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.HotPink
+        Me.ClientSize = New System.Drawing.Size(276, 349)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.MaskedTextBox2)
+        Me.Controls.Add(Me.MaskedTextBox1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Currency Converter"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents MaskedTextBox1 As MaskedTextBox
+    Friend WithEvents MaskedTextBox2 As MaskedTextBox
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Converter/Converter/Form1.vb
@@ -0,0 +1,83 @@
+﻿Public Class Form1
+
+    Dim Amount As Integer
+    Dim Rate As Double
+    Dim Answer As Double
+    Dim Update As Integer = FreeFile()
+    Dim HelpFile As Integer = FreeFile()
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        ' Add the help message here as a string.
+        Dim TempS As String = ""
+        Dim TempL As String
+        FileOpen(HelpFile, "Help.txt", OpenMode.Input)
+        Do Until EOF(HelpFile)
+            TempL = LineInput(HelpFile)
+            TempS += TempL + vbCrLf
+        Loop
+        FileClose(HelpFile)
+        MessageBox.Show(TempS)
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        ' The get rate will open the browser with this link
+        Process.Start("https://www.google.co.uk/search?safe=strict&rlz=1C1GCEA_enGB786GB786&ei=v_CTWur-DaeBgAah2JTQDA&q=1+pound+to+euro&oq=1+pound+t&gs_l=psy-ab.3.0.0i67k1l6j0l4.4779.6166.0.6894.9.6.0.3.3.0.95.442.6.6.0....0...1c.1.64.psy-ab..0.9.460...0i131k1.0.FuvkRQSMFuw")
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Amount = MaskedTextBox1.Text
+            Rate = MaskedTextBox2.Text
+            If MaskedTextBox1.Text <> "" Then
+                Answer = Rate * Convert.ToDouble(Amount)
+                TextBox1.Text = "€ " & Math.Round(Answer, 2)
+            End If
+        Catch
+            MessageBox.Show("Please enter an amount to convert")
+        End Try
+        ' Conversion and checker to see if the box is empty.
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        MaskedTextBox1.Clear()
+        TextBox1.Text = " "
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            Amount = MaskedTextBox1.Text
+            Rate = MaskedTextBox2.Text
+            If MaskedTextBox1.Text <> "" Then
+                Answer = Convert.ToDouble(Amount) / Rate
+                TextBox1.Text = "£ " & Math.Round(Answer, 2)
+            End If
+        Catch
+            MessageBox.Show("Please enter an amount to convert")
+        End Try
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        End
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        FileOpen(Update, "Update.txt", OpenMode.Output)
+        PrintLine(Update, MaskedTextBox2.Text)
+        FileClose(Update)
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim TempS As String = ""
+        Dim TempL As String
+        FileOpen(Update, "Update.txt", OpenMode.Input)
+        Do Until EOF(Update)
+            TempL = LineInput(Update)
+            TempS += TempL + vbCrLf
+        Loop
+        FileClose(Update)
+        MaskedTextBox2.Text = TempS
+        TextBox1.Text = " "
+        MaskedTextBox1.Text = " "
+    End Sub
+
+End Class
